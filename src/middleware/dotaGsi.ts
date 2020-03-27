7c1cb5a85d649bf84284ebe47b55433cb374afdd
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-27 20:14:07 +0100

    Updated config;

--- src/middleware/dotaGsi.ts
+++ src/middleware/dotaGsi.ts
@@ -19,21 +19,23 @@ class GsiClient {
 
 export const checkGSIAuth = () => async (req: Request & {client: GsiClient}, res: Response, next: NextFunction) => {
     if(!req.body.auth || !req.body.auth.token) {
+        console.log(grey('[Dota-GSI] Rejected access from ' + req.ip + ' as no auth key was given.'));
         return res.status(403).json('Foridden').end();
     }
 
     const userId = await gsiAuthTokenUnknown(req.body.auth.token);
     if(!userId) {
+        console.log(grey('[Dota-GSI] Rejected access from ' + req.ip + ' as auth key is not known.'));
         return res.status(404).json('Unknown Auth token').end();
     }
     
-    // Check if this IP is already talking to us
     for (var i = 0; i < clients.length; i++) {
-        if (clients[i].ip == req.ip) {
+        if (clients[i].ip === req.ip) {
             req.client = clients[i];
             return next();
         }
     }
+
     const newClient = new GsiClient(req.ip, req.body.auth, userId);
     clients.push(newClient);
     req.client = newClient;
