c274bd3097631ed5e2e1da686eaa2271e31ebb5a
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-05-31 12:03:49 +0200

    Fixed failure redirect;

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -9,7 +9,7 @@ const route = Router();
 export default (app: Router, passport: PassportStatic) => {
     app.use('/auth', route);
     route.get("/twitch", passport.authenticate("twitch"));
-    route.get("/twitch/callback", passport.authenticate("twitch", { failureRedirect: "/" }), (req: Request, res: Response) => {
+    route.get("/twitch/callback", passport.authenticate("twitch", { failureRedirect: "/auth/twitch" }), (req: Request, res: Response) => {
         const user = req.user as User;
         const jwtToken = jsonwebtoken.sign({sub: user.id, name: user.displayName}, config.jwtSecret);
         return res.send(jwtToken).status(200);
