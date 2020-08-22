cd0a2e16c9d5117ddfb0c7abced707cc051e1092
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-08-22 20:05:30 +0200

    Added pause processing for games;

--- src/middleware/dotaGsi.ts
+++ src/middleware/dotaGsi.ts
@@ -595,7 +595,21 @@ async function processWards(client: GsiClient, data: any): Promise<void> {
 */
 //#endregion
 
+//#region <paused>
+function getPauseKey(userId: number): string {
+    return `gsi_${userId}_gamePaused`;
+}
+async function processPause(client: GsiClient, data: any): Promise<void> {
+    const oldState = Boolean(await get(getPauseKey(client.userId)));
+    if(oldState !== Boolean(data?.map?.paused)) {
+        sendMessage(client.userId, 'pause', !oldState);
+        logFile.write(`[Dota-GSI :: ${client.displayName}] The game was ${oldState ? 'unpaused' : 'paused'} \n`);
+        await set(getPauseKey(client.userId), ''+(+!oldState));
+    }
 
+} 
+
+//#endregion
 const connectedIds = new Set();
 export async function checkGSIAuth(req: Request, res: Response, next: NextFunction) {
     if(!req.body.auth || !req.body.auth.token) {
@@ -654,6 +668,7 @@ export async function gsiBodyParser(req: Request, res: Response, next: NextFunct
     await processDeaths(client, data);
     await processPicksAndBans(client, data);
     await processItems(client, data);
+    await processPause(client, data);
     //await processWards(client, data);
 
     //Update client data
