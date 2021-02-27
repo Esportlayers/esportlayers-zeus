4a63cb9f713041e1bcd6fda8dd5bb00aab6aea8b
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-02-27 16:13:43 +0100

    Ignore bot message for sentimentic analyses;

--- src/services/twitchChat.ts
+++ src/services/twitchChat.ts
@@ -1,203 +1,283 @@
-import {ChatUserstate, Client} from 'tmi.js';
-import config from '../config';
-import { getDeaultChannels, getCustomBots, getChannelCommands, loadUserById, getUserByTrustedChannel, loadStats } from './entity/User';
-import { processCommands, clearBettingCommandsCache } from './betting/chatCommands';
-import { del, get, getObj, set, setObj } from '../loader/redis';
-import { Command } from '@streamdota/shared-types';
-import { fetchUserById } from './twitchApi';
-import { sendMessage } from './websocket';
-import processChatMessage from './wordStats';
-const tmi = require('tmi.js');
+import { ChatUserstate, Client } from "tmi.js";
+import {
+  clearBettingCommandsCache,
+  processCommands,
+} from "./betting/chatCommands";
+import { del, get, getObj, set, setObj } from "../loader/redis";
+import {
+  getChannelCommands,
+  getCustomBots,
+  getDeaultChannels,
+  getUserByTrustedChannel,
+  loadStats,
+  loadUserById,
+} from "./entity/User";
+
+import { Command } from "@streamdota/shared-types";
+import config from "../config";
+import { fetchUserById } from "./twitchApi";
+import processChatMessage from "./wordStats";
+import { sendMessage } from "./websocket";
+const tmi = require("tmi.js");
 
 const defaultConfig = {
-	options: { debug: false },
-	connection: {
-		reconnect: true,
-		secure: true
-	},
-	channels: []
+  options: { debug: false },
+  connection: {
+    reconnect: true,
+    secure: true,
+  },
+  channels: [],
 };
 
 let client: Client | null = null;
 const customInstances = new Map();
 
 async function connect(): Promise<void> {
-    if(config.twitch.defaultBotIdentity.length > 0 && config.twitch.defaultBotToken.length > 0) {
-		const channels = await getDeaultChannels();
-		client = new tmi.client({
-			...defaultConfig,
-			identity: {
-				username: config.twitch.defaultBotIdentity,
-				password: config.twitch.defaultBotToken
-			},
-			channels,
-		});
-		client!.setMaxListeners(0);
-		await client!.connect();
-		client!.on('message', messageListener);
-	}
-	
-	const customBots = await getCustomBots();
-	for(const {channel, name, password} of customBots) {
-		await createInstance('#' + channel.toLowerCase(), name, password);
-	}
+  if (
+    config.twitch.defaultBotIdentity.length > 0 &&
+    config.twitch.defaultBotToken.length > 0
+  ) {
+    const channels = await getDeaultChannels();
+    client = new tmi.client({
+      ...defaultConfig,
+      identity: {
+        username: config.twitch.defaultBotIdentity,
+        password: config.twitch.defaultBotToken,
+      },
+      channels,
+    });
+    client!.setMaxListeners(0);
+    await client!.connect();
+    client!.on("message", messageListener);
+  }
+
+  const customBots = await getCustomBots();
+  for (const { channel, name, password } of customBots) {
+    await createInstance("#" + channel.toLowerCase(), name, password);
+  }
 }
 
 connect();
 
 export function hasAccess(tags: ChatUserstate, command: Command): boolean {
-	const subscriberBadge = tags.badges && tags.badges.subscriber;
-	if(command.userAccess) return true;
-	if(command.tier1Access && tags.subscriber) return true;
-	if(command.tier2Access && subscriberBadge && subscriberBadge.length >= 4 && subscriberBadge.startsWith('2')) return true;
-	if(command.tier3Access && subscriberBadge && subscriberBadge.length >= 4 && subscriberBadge.startsWith('3')) return true;
-	if(command.vipAccess && tags.badges && tags.badges.vip) return true;
-	if(command.modAccess && tags.mod) return true;
-	if(command.streamerAccess && tags.badges && tags.badges.broadcaster) return true;
-	return false;
+  const subscriberBadge = tags.badges && tags.badges.subscriber;
+  if (command.userAccess) return true;
+  if (command.tier1Access && tags.subscriber) return true;
+  if (
+    command.tier2Access &&
+    subscriberBadge &&
+    subscriberBadge.length >= 4 &&
+    subscriberBadge.startsWith("2")
+  )
+    return true;
+  if (
+    command.tier3Access &&
+    subscriberBadge &&
+    subscriberBadge.length >= 4 &&
+    subscriberBadge.startsWith("3")
+  )
+    return true;
+  if (command.vipAccess && tags.badges && tags.badges.vip) return true;
+  if (command.modAccess && tags.mod) return true;
+  if (command.streamerAccess && tags.badges && tags.badges.broadcaster)
+    return true;
+  return false;
 }
 
-async function replacePlaceholder(message: string, tags: ChatUserstate, channel: string): Promise<string> {
-	let fullMessage = message;
-	if(tags["display-name"]) {
-		fullMessage = fullMessage.replace(/\{USER\}/g, tags["display-name"]);
-	}
+async function replacePlaceholder(
+  message: string,
+  tags: ChatUserstate,
+  channel: string
+): Promise<string> {
+  let fullMessage = message;
+  if (tags["display-name"]) {
+    fullMessage = fullMessage.replace(/\{USER\}/g, tags["display-name"]);
+  }
 
-	if(message.includes('{TOTAL_GAMES}') || message.includes('{GAMES_WON}') || message.includes('{GAMES_LOST}')) {
-		const {id} = await getUserByTrustedChannel(channel);
-		const userStats = await loadStats(id);
-		const wins = userStats.filter(({won}) => won).length;
+  if (
+    message.includes("{TOTAL_GAMES}") ||
+    message.includes("{GAMES_WON}") ||
+    message.includes("{GAMES_LOST}")
+  ) {
+    const { id } = await getUserByTrustedChannel(channel);
+    const userStats = await loadStats(id);
+    const wins = userStats.filter(({ won }) => won).length;
 
-		fullMessage = fullMessage.replace(/\{TOTAL_GAMES\}/g, '' + userStats.length);
-		fullMessage = fullMessage.replace(/\{GAMES_WON\}/g, '' + wins);
-		fullMessage = fullMessage.replace(/\{GAMES_LOST\}/g, '' + (userStats.length - wins));
-	}
+    fullMessage = fullMessage.replace(
+      /\{TOTAL_GAMES\}/g,
+      "" + userStats.length
+    );
+    fullMessage = fullMessage.replace(/\{GAMES_WON\}/g, "" + wins);
+    fullMessage = fullMessage.replace(
+      /\{GAMES_LOST\}/g,
+      "" + (userStats.length - wins)
+    );
+  }
 
-	return fullMessage;
+  return fullMessage;
 }
 
 export interface ChannelCommand {
-	[x: string]: Command;
+  [x: string]: Command;
 }
-export function getCommandsCacheKey(channel: string, types: string[], type: 'entity' | 'commands' = 'entity'): string {
-	return `commands_${channel.toLowerCase()}_${type}_${types.join('-')}`;
+export function getCommandsCacheKey(
+  channel: string,
+  types: string[],
+  type: "entity" | "commands" = "entity"
+): string {
+  return `commands_${channel.toLowerCase()}_${type}_${types.join("-")}`;
 }
 
 export function keywordListenerKey(channel: string): string {
-	return `keyword_listener${channel.toLowerCase()}`;
+  return `keyword_listener${channel.toLowerCase()}`;
 }
 
-async function messageListener(channel: string, tags: ChatUserstate, message: string, self: boolean) {
-	if(self) return;
+async function messageListener(
+  channel: string,
+  tags: ChatUserstate,
+  message: string,
+  self: boolean
+) {
+  if (self) return;
 
-	let userCommands = await getObj<ChannelCommand>(getCommandsCacheKey(channel, ['default', 'dotaWinLoss']));
+  let userCommands = await getObj<ChannelCommand>(
+    getCommandsCacheKey(channel, ["default", "dotaWinLoss"])
+  );
 
-	if(!userCommands) {
-		const commands = await getChannelCommands(channel, new Set(['default', 'dotaWinLoss']));
-		const mapped = commands.reduce<ChannelCommand>((acc, command) => {
-			if(command.active) {
-				acc[command.command.toLowerCase()] = command;
-			}
-			return acc;
-		}, {});
-		await setObj(getCommandsCacheKey(channel, ['default', 'dotaWinLoss']), mapped);
-		const commandKeys = commands.map(({command}) => command.toLowerCase());
-		await setObj(getCommandsCacheKey(channel, ['default', 'dotaWinLoss'], 'commands'), commandKeys);
-		userCommands = mapped;
-	}
+  if (!userCommands) {
+    const commands = await getChannelCommands(
+      channel,
+      new Set(["default", "dotaWinLoss"])
+    );
+    const mapped = commands.reduce<ChannelCommand>((acc, command) => {
+      if (command.active) {
+        acc[command.command.toLowerCase()] = command;
+      }
+      return acc;
+    }, {});
+    await setObj(
+      getCommandsCacheKey(channel, ["default", "dotaWinLoss"]),
+      mapped
+    );
+    const commandKeys = commands.map(({ command }) => command.toLowerCase());
+    await setObj(
+      getCommandsCacheKey(channel, ["default", "dotaWinLoss"], "commands"),
+      commandKeys
+    );
+    userCommands = mapped;
+  }
 
-	const commandKeys = (await getObj<string[]>(getCommandsCacheKey(channel, ['default', 'dotaWinLoss'], 'commands')))!;
-	const lowerMessage = message.toLowerCase();
-	if(new Set(commandKeys).has(lowerMessage) && hasAccess(tags, userCommands[lowerMessage])) {
-		const msg = await replacePlaceholder(userCommands[lowerMessage].message, tags, channel)
-		publish(channel, msg);
-	}
+  const commandKeys = (await getObj<string[]>(
+    getCommandsCacheKey(channel, ["default", "dotaWinLoss"], "commands")
+  ))!;
+  const lowerMessage = message.toLowerCase();
+  if (
+    new Set(commandKeys).has(lowerMessage) &&
+    hasAccess(tags, userCommands[lowerMessage])
+  ) {
+    const msg = await replacePlaceholder(
+      userCommands[lowerMessage].message,
+      tags,
+      channel
+    );
+    publish(channel, msg);
+  }
 
-	let keyword = await get(keywordListenerKey(channel));
+  let keyword = await get(keywordListenerKey(channel));
 
-	if(!keyword || keyword !== 'none') {
-		const {id} = await getUserByTrustedChannel(channel);
-		const user = await loadUserById(id);
-		keyword = user?.useKeywordListener && user?.keywordListener || 'none';
-		await set(keywordListenerKey(channel), keyword);
-	}
+  if (!keyword || keyword !== "none") {
+    const { id } = await getUserByTrustedChannel(channel);
+    const user = await loadUserById(id);
+    keyword = (user?.useKeywordListener && user?.keywordListener) || "none";
+    await set(keywordListenerKey(channel), keyword);
+  }
 
-	if(keyword !== 'none' && lowerMessage.includes(keyword) && tags['user-id']) {
-		const twitchUser = await fetchUserById(tags['user-id']);
-		const {id} = await getUserByTrustedChannel(channel);
-		sendMessage(id, 'keyword_message', {
-			message,
-			name: twitchUser.display_name,
-			logo: twitchUser.logo,
-			time: tags['tmi-sent-ts'],
-		});
-	}
-	await processChatMessage(channel, message);
+  if (keyword !== "none" && lowerMessage.includes(keyword) && tags["user-id"]) {
+    const twitchUser = await fetchUserById(tags["user-id"]);
+    const { id } = await getUserByTrustedChannel(channel);
+    sendMessage(id, "keyword_message", {
+      message,
+      name: twitchUser.display_name,
+      logo: twitchUser.logo,
+      time: tags["tmi-sent-ts"],
+    });
+  }
+  await processChatMessage(channel, message, tags["username"]!);
 
-	processCommands(channel, tags, message);
+  processCommands(channel, tags, message);
 }
 
 export async function resetKeywordListener(userId: number): Promise<void> {
-	const {displayName}  = (await loadUserById(userId))!;
-	const fullChannel = '#' + displayName.toLowerCase();
-	await del(keywordListenerKey(fullChannel));
+  const { displayName } = (await loadUserById(userId))!;
+  const fullChannel = "#" + displayName.toLowerCase();
+  await del(keywordListenerKey(fullChannel));
 }
 
 export function joinChannel(channel: string): void {
-    client && client.join(channel);
+  client && client.join(channel);
 }
 
 export function partChannel(channel: string): void {
-	//@ts-ignore
-	if(client && client.getChannels().includes(channel)) {
-		client.part(channel);
-	}
+  //@ts-ignore
+  if (client && client.getChannels().includes(channel)) {
+    client.part(channel);
+  }
 }
 
 export function publish(channel: string, message: string): void {
-	if(customInstances.has(channel)) {
-		customInstances.get(channel).say(channel, message);
-	} else {
-		client && client.readyState() === 'OPEN' && client.say(channel, message);
-	}
+  if (customInstances.has(channel)) {
+    customInstances.get(channel).say(channel, message);
+  } else {
+    client && client.readyState() === "OPEN" && client.say(channel, message);
+  }
 }
 
-export async function createInstance(channel: string, username: string, password: string): Promise<void> {
-	await deleteInstance(channel);
-	partChannel(channel);
+export async function createInstance(
+  channel: string,
+  username: string,
+  password: string
+): Promise<void> {
+  await deleteInstance(channel);
+  partChannel(channel);
 
-	const instance = new tmi.client({
-		...defaultConfig,
-		identity: {username, password},
-		channels:[channel]
-	});
-	instance.setMaxListeners(0);
-	instance.connect();
-	instance.on('message', messageListener);
-	customInstances.set(channel, instance);
+  const instance = new tmi.client({
+    ...defaultConfig,
+    identity: { username, password },
+    channels: [channel],
+  });
+  instance.setMaxListeners(0);
+  instance.connect();
+  instance.on("message", messageListener);
+  customInstances.set(channel, instance);
 }
 
 export async function deleteInstance(channel: string): Promise<void> {
-	if(customInstances.has(channel)) {
-		await customInstances.get(channel).disconnect();
-		customInstances.delete(channel);
-	}
+  if (customInstances.has(channel)) {
+    await customInstances.get(channel).disconnect();
+    customInstances.delete(channel);
+  }
 }
 
 export async function clearUserCommandsChache(userId: number): Promise<void> {
-	const {displayName}  = (await loadUserById(userId))!;
-	const fullChannel = '#' + displayName.toLowerCase();
+  const { displayName } = (await loadUserById(userId))!;
+  const fullChannel = "#" + displayName.toLowerCase();
 
-	await setObj(getCommandsCacheKey(fullChannel, ['default', 'dotaWinLoss']), null);
-	await setObj(getCommandsCacheKey(fullChannel, ['default', 'dotaWinLoss'], 'commands'), null);
+  await setObj(
+    getCommandsCacheKey(fullChannel, ["default", "dotaWinLoss"]),
+    null
+  );
+  await setObj(
+    getCommandsCacheKey(fullChannel, ["default", "dotaWinLoss"], "commands"),
+    null
+  );
 
-	clearBettingCommandsCache(fullChannel);
+  clearBettingCommandsCache(fullChannel);
 }
 
 export function getChannels(): string[] {
-	if(client) {
-		return [...client.getChannels(), ...customInstances.keys()];
-	}
+  if (client) {
+    return [...client.getChannels(), ...customInstances.keys()];
+  }
 
-	return [];
+  return [];
 }
--- src/services/wordStats.ts
+++ src/services/wordStats.ts
@@ -1,67 +1,90 @@
-import { Word } from "@streamdota/shared-types";
-import { getObj, setObj } from "../loader/redis";
 import { createWordMessage, getUserWordGroups } from "./entity/ChatKeyWords";
-import { getUserByTrustedChannel } from "./entity/User";
-import { fetchSentimentScore } from "./naixApi";
+import { getObj, setObj } from "../loader/redis";
+
+import { Word } from "@streamdota/shared-types";
 import { fetchChatterCount } from "./twitchApi";
+import { fetchSentimentScore } from "./naixApi";
+import { getUserByTrustedChannel } from "./entity/User";
 
 const getKeywordListKey = (id: number) => `user_${id}_word_listening_list`;
 const getWordByName = (name: string, id: number) => `user_${id}_word_${name}`;
 
 async function getUserKeyWords(id: number): Promise<string[]> {
-    let keywords = await getObj<string[]>(getKeywordListKey(id));
-    if(!keywords) {
-        const wordGroups = await getUserWordGroups(id);
-        const list: string[] = [];
+  let keywords = await getObj<string[]>(getKeywordListKey(id));
+  if (!keywords) {
+    const wordGroups = await getUserWordGroups(id);
+    const list: string[] = [];
 
-        for(const group of wordGroups) {
-            if(group.active) {
-                for(const word of group.words) {
-                    const lower = word.word.toLowerCase();
-                    list.push(lower);
-                    await setObj(getWordByName(lower, id), word);
-                }
-            }
+    for (const group of wordGroups) {
+      if (group.active) {
+        for (const word of group.words) {
+          const lower = word.word.toLowerCase();
+          list.push(lower);
+          await setObj(getWordByName(lower, id), word);
         }
-
-        await setObj(getKeywordListKey(id), list);
-        keywords = list;
+      }
     }
 
-    return keywords;
+    await setObj(getKeywordListKey(id), list);
+    keywords = list;
+  }
+
+  return keywords;
 }
 
-async function saveMessageForKeyword(keyword: string, message: string, streamerName: string, id: number): Promise<void> {
-    const chatters = await fetchChatterCount(streamerName);
-    const word = await getObj<Word>(getWordByName(keyword, id));
-    if(word) {
-        let score = 0, magnitude = 0;
-        if(Boolean(word.useSentimentAnalysis)) {
-            const data = await fetchSentimentScore(message);
-            score = data.score;
-            magnitude = data.magnitude;
-        }
-        await createWordMessage(word.id, message, chatters, score, magnitude)
+async function saveMessageForKeyword(
+  keyword: string,
+  message: string,
+  streamerName: string,
+  id: number,
+  ignoreAnalyses?: boolean
+): Promise<void> {
+  const chatters = await fetchChatterCount(streamerName);
+  const word = await getObj<Word>(getWordByName(keyword, id));
+  if (word) {
+    let score = 0,
+      magnitude = 0;
+    if (!ignoreAnalyses && Boolean(word.useSentimentAnalysis)) {
+      const data = await fetchSentimentScore(message);
+      score = data.score;
+      magnitude = data.magnitude;
     }
+    await createWordMessage(word.id, message, chatters, score, magnitude);
+  }
 }
 
-
-export default async function processChatMessage(channel: string, message: string): Promise<void> {
-    const {id} = await getUserByTrustedChannel(channel);
-    const channelKeyWords = await getUserKeyWords(id);
-    const messageLower = message.toLowerCase();
-    const matches = channelKeyWords.filter((keyword) => messageLower.indexOf(keyword) !== -1);
-    if(matches.length > 0) {
-        for(const match of matches) {
-            await saveMessageForKeyword(match, message, channel.substr(1), id);
-        }
+const ignoredAnalysesUsernames = new Set(["streamelements"]);
+export default async function processChatMessage(
+  channel: string,
+  message: string,
+  username: string
+): Promise<void> {
+  const { id } = await getUserByTrustedChannel(channel);
+  const channelKeyWords = await getUserKeyWords(id);
+  const messageLower = message.toLowerCase();
+  const matches = channelKeyWords.filter(
+    (keyword) => messageLower.indexOf(keyword) !== -1
+  );
+  if (matches.length > 0) {
+    for (const match of matches) {
+      await saveMessageForKeyword(
+        match,
+        message,
+        channel.substr(1),
+        id,
+        ignoredAnalysesUsernames.has(username)
+      );
     }
+  }
 }
 
 export async function resetUserStorage(id: number): Promise<void> {
-    await setObj(getKeywordListKey(id), null);
+  await setObj(getKeywordListKey(id), null);
 }
 
-export async function resetWordStorage(id: number, name: string): Promise<void> {
-    await setObj(getWordByName(name.toLowerCase(), id), null);
+export async function resetWordStorage(
+  id: number,
+  name: string
+): Promise<void> {
+  await setObj(getWordByName(name.toLowerCase(), id), null);
 }
