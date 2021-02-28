39e6382850c39a3ccee804942c841e9f8af495c4
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-02-28 10:37:46 +0100

    Added keyword to overlay message;

--- src/api/routes/live.ts
+++ src/api/routes/live.ts
@@ -1,36 +1,49 @@
-import { Request, Response, Router } from 'express';
+import { Request, Response, Router } from "express";
 
-import { User } from '@streamdota/shared-types';
-import { fetchMatchTeams } from '../../services/steamWebApi';
-import { requireAuthorization } from '../../middleware/requireAuthorization';
-import { sendMessage } from '../../services/websocket';
+import { User } from "@streamdota/shared-types";
+import { fetchMatchTeams } from "../../services/steamWebApi";
+import { requireAuthorization } from "../../middleware/requireAuthorization";
+import { sendMessage } from "../../services/websocket";
 
 const route = Router();
 
 export default (app: Router) => {
-    app.use('/live', route);
+  app.use("/live", route);
 
-    route.get('/teams/:matchId', requireAuthorization, async (req: Request, res: Response) => {
-        const teamsData = await fetchMatchTeams(+req.params.matchId);
-        if(teamsData) {
-            return res.json({id: +req.params.matchId, ...teamsData}).status(200);
-        }
-        return res.json({
-            id: +req.params.matchId,
-            radiant: {
-                name: 'Radiant',
-                logo: null,
-            },
-            dire: {
-                name: 'Dire',
-                logo: null,
-            }
-        }).status(200);
-    });
-    
-    route.post('/keywordQuestion', requireAuthorization, async (req: Request, res: Response) => {
-		sendMessage((req.user as User).id, 'keyword_message_overlay', req.body);
+  route.get(
+    "/teams/:matchId",
+    requireAuthorization,
+    async (req: Request, res: Response) => {
+      const teamsData = await fetchMatchTeams(+req.params.matchId);
+      if (teamsData) {
+        return res.json({ id: +req.params.matchId, ...teamsData }).status(200);
+      }
+      return res
+        .json({
+          id: +req.params.matchId,
+          radiant: {
+            name: "Radiant",
+            logo: null,
+          },
+          dire: {
+            name: "Dire",
+            logo: null,
+          },
+        })
+        .status(200);
+    }
+  );
 
-        return res.sendStatus(204);
-    });
+  route.post(
+    "/keywordQuestion",
+    requireAuthorization,
+    async (req: Request, res: Response) => {
+      sendMessage((req.user as User).id, "keyword_message_overlay", {
+        ...req.body,
+        keyword: (req.user as User).keywordListener,
+      });
+
+      return res.sendStatus(204);
+    }
+  );
 };
