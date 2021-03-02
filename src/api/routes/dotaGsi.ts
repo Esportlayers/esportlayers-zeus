d1a4c12c6da2d053569cd203cc17e9d92243bae5
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-03-02 16:33:22 +0100

    Testing minimal ws connection;

--- src/api/routes/dotaGsi.ts
+++ src/api/routes/dotaGsi.ts
@@ -1,50 +1,81 @@
-import { Router, Request, Response } from 'express';
-import { requireAuthorization } from '../../middleware/requireAuthorization';
-import { createGsiAuthToken, resetDotaGsi } from '../../services/entity/User';
-import {User} from '@streamdota/shared-types';
-import { generateConfig } from '../../services/gsiConfigGenerator';
-import path from 'path';
-import ws from 'ws';
-import { heartbeat } from '../../tasks/websocketHeartbeat';
-import { checkUserFrameWebsocketApiKey } from '../../middleware/frameApi';
-import { parseEvents } from '@esportlayers/morphling';
-import { sendMessage } from '../../services/websocket';
-import { checkGSIAuthToken, handleMorphlingEvents, newGSIListener } from '../../middleware/gsiConnection';
+import { Request, Response, Router } from "express";
+import {
+  checkGSIAuthToken,
+  handleMorphlingEvents,
+  newGSIListener,
+} from "../../middleware/gsiConnection";
+import { createGsiAuthToken, resetDotaGsi } from "../../services/entity/User";
+
+import { User } from "@streamdota/shared-types";
+import { checkUserFrameWebsocketApiKey } from "../../middleware/frameApi";
+import { generateConfig } from "../../services/gsiConfigGenerator";
+import { heartbeat } from "../../tasks/websocketHeartbeat";
+import { parseEvents } from "@esportlayers/morphling";
+import path from "path";
+import { requireAuthorization } from "../../middleware/requireAuthorization";
+import { sendMessage } from "../../services/websocket";
+import ws from "ws";
 
 const route = Router();
 
 export default (app: Router) => {
-  app.use('/dota-gsi', route);
+  app.use("/dota-gsi", route);
 
-  route.post('/', checkGSIAuthToken, async (req: Request, res: Response) => {
+  route.post("/", checkGSIAuthToken, async (req: Request, res: Response) => {
     const user = req.user as User;
-    const events = await parseEvents(req.body, '' + user.id);
+    const events = await parseEvents(req.body, "" + user.id);
     await handleMorphlingEvents(events, user.id);
-    for(const {event, value} of events) {
+    for (const { event, value } of events) {
       sendMessage(user.id, event, value);
     }
-    return res.end()
+    return res.end();
   });
 
-  route.get('/generateConfig', requireAuthorization, async (req: Request, res: Response) => {
-    const user = req.user as User;
-    const gsiAuth = await createGsiAuthToken(user.id);
-    const [filename, configPath] = generateConfig(gsiAuth, user.displayName);
+  route.get(
+    "/generateConfig",
+    requireAuthorization,
+    async (req: Request, res: Response) => {
+      const user = req.user as User;
+      const gsiAuth = await createGsiAuthToken(user.id);
+      const [filename, configPath] = generateConfig(gsiAuth, user.displayName);
 
-    res.set({"Content-Disposition":`attachment; filename=${filename}`});
-    res.sendFile(path.resolve(configPath));
-  });
+      res.set({ "Content-Disposition": `attachment; filename=${filename}` });
+      res.sendFile(path.resolve(configPath));
+    }
+  );
 
-  route.delete('/resetGsi', requireAuthorization, async (req: Request, res: Response) => {
-    const user = req.user as User;
-    await resetDotaGsi(user.id);
+  route.delete(
+    "/resetGsi",
+    requireAuthorization,
+    async (req: Request, res: Response) => {
+      const user = req.user as User;
+      await resetDotaGsi(user.id);
 
-    return res.sendStatus(204);
-  });
+      return res.sendStatus(204);
+    }
+  );
 
-  route.ws('/live/:frameApiKey', checkUserFrameWebsocketApiKey, newGSIListener, (conn: ws) => {
-    //@ts-ignore
-    conn.isAlive = true;
-    conn.on('pong', heartbeat);
-  });
+  route.ws(
+    "/live/:frameApiKey",
+    checkUserFrameWebsocketApiKey,
+    newGSIListener,
+    (conn: ws) => {
+      //@ts-ignore
+      conn.isAlive = true;
+      conn.on("pong", heartbeat);
+    }
+  );
+
+  route.ws(
+    "/liveMinimal/:frameApiKey",
+    checkUserFrameWebsocketApiKey,
+    newGSIListener,
+    (conn: ws) => {
+      //@ts-ignore
+      conn.isAlive = true;
+      //@ts-ignore
+      conn.noGsiEvents = true;
+      conn.on("pong", heartbeat);
+    }
+  );
 };
--- src/services/websocket.ts
+++ src/services/websocket.ts
@@ -1,7 +1,14 @@
 import { WebsocketInstance } from "..";
 
 export function sendMessage(userId: number, type: string, value: any): void {
-    //@ts-ignore
-    const connections = [...WebsocketInstance.getWss().clients.values()].filter((c) => c.user && (c.user.id === userId));
-    connections.forEach((client) => client.send(JSON.stringify({type, value })));
+  const connections = [...WebsocketInstance.getWss().clients.values()].filter(
+    (c) =>
+      //@ts-ignore
+      c.user &&
+      //@ts-ignore
+      c.user.id === userId &&
+      //@ts-ignore
+      (!type.includes("gsi") || !c.noGsiEvents)
+  );
+  connections.forEach((client) => client.send(JSON.stringify({ type, value })));
 }
