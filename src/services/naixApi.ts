11ed9b797f0186b2ab8f516333045ccac29d5a45
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-03-12 19:39:14 +0100

    Adjusted naix api;

--- src/services/naixApi.ts
+++ src/services/naixApi.ts
@@ -1,21 +1,19 @@
-import fetch from 'node-fetch';
-import config from '../config';
+import config from "../config";
+import fetch from "node-fetch";
 
-const apiUrl = 'https://naix.streamdota.com/';
+const apiUrl = "https://naix.streamdota.com/";
 
+export async function fetchSentimentScore(
+  message: string
+): Promise<{ score: number; magnitude: number }> {
+  const response = await fetch(apiUrl + `?apiKey=${config.naixApiKey}`, {
+    method: "POST",
+    body: JSON.stringify({ message }),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+    return await response.json();
+  }
 
-export async function fetchSentimentScore(message: string): Promise<{score: number, magnitude: number}> {
-    const response = await fetch(
-        apiUrl + `?apiKey=${config.naixApiKey}`, 
-        {
-            method: 'POST',
-            body:    JSON.stringify({message}),
-            headers: { 'Content-Type': 'application/json' },
-        },
-    );
-    if(response.ok) {
-        return await response.json();
-    }
-
-    return {score: 0, magnitude: 0};
+  return { score: 0, magnitude: 0 };
 }
