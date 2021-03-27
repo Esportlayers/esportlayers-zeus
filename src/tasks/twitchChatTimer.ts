2594e063a00455a8a61a3c826f615ba0c2cadf3b
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2021-03-27 07:50:03 +0100

    Fixed chat timer;

--- src/tasks/twitchChatTimer.ts
+++ src/tasks/twitchChatTimer.ts
@@ -1,55 +1,61 @@
 import { getChannels, publish } from "../services/twitchChat";
 import { getUserByTrustedChannel, loadUserById } from "../services/entity/User";
-import { getUserTimer } from "../services/entity/Timer";
+
 import dayjs from "dayjs";
+import { getUserTimer } from "../services/entity/Timer";
 
 interface Timer {
-    message: string;
-    period: number;
-    lastPublish: number;
+  message: string;
+  period: number;
+  lastPublish: number;
 }
 
 const channelTimer = new Map<string, Timer[]>();
 
 async function checkTimerUpdates(): Promise<void> {
-    const channelsToCheck = getChannels();
-    const currentTS = dayjs().unix();
+  const channelsToCheck = getChannels();
+  const currentTS = dayjs().unix();
 
-    for(const channel of channelsToCheck) {
-        if(!channelTimer.has(channel)) {
-            const {id} = await getUserByTrustedChannel(channel);
-            const rawTimer = await getUserTimer(id);
-            const timer: Timer[] = rawTimer.filter(({active}) => active).map(({message, period}) => {
-                const min = .6 * period;
-                return {
-                    period,
-                    message,
-                    lastPublish: currentTS + Math.floor(min + Math.random() * (period - min)),
-                };
-            });
+  for (const channel of channelsToCheck) {
+    if (!channelTimer.has(channel)) {
+      const { id } = (await getUserByTrustedChannel(channel)) || { id: null };
+      if (id) {
+        const rawTimer = await getUserTimer(id);
+        const timer: Timer[] = rawTimer
+          .filter(({ active }) => active)
+          .map(({ message, period }) => {
+            const min = 0.6 * period;
+            return {
+              period,
+              message,
+              lastPublish:
+                currentTS + Math.floor(min + Math.random() * (period - min)),
+            };
+          });
 
-            channelTimer.set(channel, timer);
-        }
-
-        const timers = channelTimer.get(channel) || [];
-        for(const timer of timers) {
-            if(timer.lastPublish <= currentTS) {
-                timer.lastPublish = currentTS + timer.period;
-                publish(channel, timer.message);
-                break;
-            }
-        }
+        channelTimer.set(channel, timer);
+      }
     }
+
+    const timers = channelTimer.get(channel) || [];
+    for (const timer of timers) {
+      if (timer.lastPublish <= currentTS) {
+        timer.lastPublish = currentTS + timer.period;
+        publish(channel, timer.message);
+        break;
+      }
+    }
+  }
 }
 
-if(process.env.NODE_ENV !== 'test') {
-    setInterval(checkTimerUpdates, 20000);
-} 
+if (process.env.NODE_ENV !== "test") {
+  setInterval(checkTimerUpdates, 20000);
+}
 
 export async function clearChannelCache(userId: number): Promise<void> {
-	const {displayName}  = (await loadUserById(userId))!;
-    const fullChannel = '#' + displayName.toLowerCase();
-    if(channelTimer.has(fullChannel)) {
-        channelTimer.delete(fullChannel);
-    }
+  const { displayName } = (await loadUserById(userId))!;
+  const fullChannel = "#" + displayName.toLowerCase();
+  if (channelTimer.has(fullChannel)) {
+    channelTimer.delete(fullChannel);
+  }
 }
